3bfe431ba3ed57f02de636090c162dfffdc8cdef
claire froelich <claire.froelich@gmail.com>
2020-05-17 14:57:13 -0400

    restructure navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import SVG from 'react-inlinesvg'
 
 import imgNav from '../../static/images/accents/nav.png'
+import iconArrow from '../../static/icons/arrow.svg'
 
 const Navbar = ({}) => (
-  <div className="navbar" style={{ backgroundImage: `url('${imgNav}')`}}>
-
+  <div className="navbar">
+    <div className="navbar-background" style={{ backgroundImage: `url('${imgNav}')`}}>
+    </div>
+    <button id="navbar-toggle">
+      <SVG src={iconArrow} />
+    </button>
   </div>
+
 )
 
 export default Navbar

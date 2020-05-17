a005d80e100d972d53cedcc6c6e15606ab95e3a5
claire froelich <claire.froelich@gmail.com>
2020-05-17 15:40:43 -0400

    accessible focus outline

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SVG from 'react-inlinesvg'
+
+
+import AccessibleFocusOutline from '../util/components/accessibleFocusOutline'
 
 import imgNav from '../../static/images/accents/nav.png'
 import iconArrow from '../../static/icons/arrow.svg'
 
-const Navbar = ({}) => (
-  <div className="navbar">
-    <div className="navbar-background" style={{ backgroundImage: `url('${imgNav}')`}}>
-    </div>
-    <button id="navbar-toggle">
-      <SVG src={iconArrow} />
-    </button>
-  </div>
+const Navbar = () => {
+  const [open, setOpen] = useState(false)
+  const [y, setY] = useState(0)
+  const [deg, setDeg] = useState(0)
 
-)
+  useEffect(()=> {
+    setY(open ? 100 : 0)
+    setDeg(open ? 180 : 0)
+  },[open])
+
+  const toggleMenu = () => {
+    setOpen(!open)
+  }
+
+  const navStyle = {
+    transform: `translateY(${y}px)`,
+    transition: 'transform ease-in 0.3s'
+  }
+
+  const arrowStyle = {
+    transform: `rotate(${deg}deg)`,
+    transition: 'transform ease 0.3s'
+  }
+
+  return (
+    <div className="navbar" style={navStyle}>
+      <div className="navbar-background" style={{ backgroundImage: `url('${imgNav}')` }}>
+      </div>
+      <AccessibleFocusOutline>
+        <button id="navbar-toggle" style={arrowStyle} onClick={toggleMenu}>
+          <SVG src={iconArrow} />
+        </button>
+      </AccessibleFocusOutline>
+
+    </div>
+  )
+}
+
+
 
 export default Navbar
---
+++ src/util/components/accessibleFocusOutline.js
@@ -0,0 +1,39 @@
+// makes child component's :active state outline invisible unless accessed by tab
+
+// inspired by Sahar Bala
+// https://www.eventbrite.com/engineering/how-to-fix-the-ugly-focus-ring-and-not-break-accessibility-in-react/
+
+import React, { useState, useEffect } from 'react'
+
+const AccessibleFocusOutline = ({ children }) => {
+  const [outlineEnabled, setOutlineEnabled] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown)
+    return () => window.removeEventListener('keydown', handleKeydown)
+  },[])
+
+  useEffect(() => {
+    window.addEventListener('click', handleClick)
+    return () => window.removeEventListener('click', handleClick)
+  },[])
+
+  const handleKeydown = (e) => {
+    const isTabEvent = e.keyCode === 9 || e.keyCode === 13
+    if (isTabEvent) {
+      setOutlineEnabled(true)
+    }
+  }
+
+  const handleClick = (e) => {
+    setOutlineEnabled(false)
+  }
+
+  return (
+    <span className={ outlineEnabled ? '' : 'no-outline-on-focus' }>
+      {children}
+    </span>
+  )
+}
+
+export default AccessibleFocusOutline

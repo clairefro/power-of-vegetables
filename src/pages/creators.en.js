881e40a6e06dd87bca8a687bfdc3b4bac313acac
claire froelich <claire.froelich@gmail.com>
2020-05-21 20:33:10 -0400

    add fade  animation creators

--- src/pages/creators.en.js
+++ src/pages/creators.en.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SVG from 'react-inlinesvg'
-
+import Fade from 'react-reveal/Fade'
 import SEO from '../components/seo'
 
 // images
@@ -17,34 +17,36 @@ export default () => (
       description="'The Power of Vegetables', written by Claire Froelich and illustrated by Varvara Fomina."
     />
     <div className="container">
-      <h2 className="ta-center">About the Creators</h2>
+      <Fade>
+        <h2 className="ta-center">About the Creators</h2>
 
-      <div className="creator">
-        <div className="creator-section-img">
-          <img src={imgClaire} className="responsive" alt="Claire Froelich"/>
+        <div className="creator">
+          <div className="creator-section-img">
+            <img src={imgClaire} className="responsive" alt="Claire Froelich"/>
+          </div>
+          <div className="creator-section-text">
+            <h3><strong>Claire Froelich</strong></h3>
+            <p><strong>Author</strong></p>
+            <p>Moved by the funny things in life, inspired by her brother's comedic tooting at the dinner table. Also a web developer, she made this website.</p>
+          </div>
         </div>
-        <div className="creator-section-text">
-          <h3><strong>Claire Froelich</strong></h3>
-          <p><strong>Author</strong></p>
-          <p>Moved by the funny things in life, inspired by her brother's comedic tooting at the dinner table. Also a web developer, she made this website.</p>
+      </Fade>
+      <Fade>
+        <div className="creator reverse">
+          <div className="creator-section-img">
+            <img src={imgVarvara} className="responsive" alt="Varvara Fomina"/>
+          </div>
+          <div className="creator-section-text">
+            <h3><strong>Varvara Fomina</strong></h3>
+            <p><strong>Illustrator</strong></p>
+            <p>Enthusiastic freelance illustrator whose passion is to dive into others' storytelling, whether it be tooting deer or nuclear science.</p>
+            <a href="https://www.instagram.com/barbara.fomina/" target="_blank" className="link d-flex" style={{ alignItems: 'center'}}>
+              <SVG src={iconInstagram}/>
+              <span style={{marginLeft: '0.5rem'}}>@barbara.fomina</span>
+            </a>
+          </div>
         </div>
-      </div>
-
-      <div className="creator reverse">
-        <div className="creator-section-img">
-          <img src={imgVarvara} className="responsive" alt="Varvara Fomina"/>
-        </div>
-        <div className="creator-section-text">
-          <h3><strong>Varvara Fomina</strong></h3>
-          <p><strong>Illustrator</strong></p>
-          <p>Enthusiastic freelance illustrator whose passion is to dive into others' storytelling, whether it be tooting deer or nuclear science.</p>
-          <a href="https://www.instagram.com/barbara.fomina/" target="_blank" className="link d-flex" style={{ alignItems: 'center'}}>
-            <SVG src={iconInstagram}/>
-            <span style={{marginLeft: '0.5rem'}}>@barbara.fomina</span>
-          </a>
-        </div>
-      </div>
-
+      </Fade>
     </div>
   </div>
 )
